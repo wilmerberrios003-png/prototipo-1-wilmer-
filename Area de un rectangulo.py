b550fdcfb3a3443803af6f0ab8e0f9a6dc5db48d
wilmerberrios003-png <wilmerberrios003@gmail.com>
2026-06-06 14:20:35 -0600

    Add files via upload

---
+++ Area de un rectangulo.py
@@ -0,0 +1,13 @@
+# Ejercicio 2: Área de un rectángulo
+
+# Pedir la base del rectángulo
+base = float(input("Ingrese la base del rectángulo: "))
+
+# Pedir la altura del rectángulo
+altura = float(input("Ingrese la altura del rectángulo: "))
+
+# Calcular el área
+area = base * altura
+
+# Mostrar el resultado
+print("El área del rectángulo es:", area)

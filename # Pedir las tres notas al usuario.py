4d50ab861917a2e09b5c12fdb6081aa87376b9da
wilmerberrios003-png <wilmerberrios003@gmail.com>
2026-06-06 13:45:37 -0600

    Add files via upload

---
+++ # Pedir las tres notas al usuario.py
@@ -0,0 +1,10 @@
+# Pedir las tres notas al usuario
+nota1 = float(input("Ingrese la primera nota: "))
+nota2 = float(input("Ingrese la segunda nota: "))
+nota3 = float(input("Ingrese la tercera nota: "))
+
+# Calcular el promedio
+promedio = (nota1 + nota2 + nota3) / 3
+
+# Mostrar el resultado
+print("El promedio es:", promedio)
